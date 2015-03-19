eae4c1be17696b7d5e874d1f4dcafd4a0f6f0584
Victoriano Navarro <vitiman@gmail.com>
2015-03-19 23:37:00 +0100

    Added config file and modularize the project

--- imap_checker.py
+++ imap_checker.py
@@ -8,62 +8,93 @@
 # Notifications
 # http://crysol.org/es/node/1356
 
+
+# http://stackoverflow.com/questions/4281821/pynotify-not-working-from-cron
+
+
 # python imports
+import ConfigParser
 import imaplib
+import logging
+import os
 from email.parser import HeaderParser
 
 # 3rd. libraries imports
 import pynotify
 
+logger = logging.getLogger(__name__)
 
-def fetch_mail():
-    mail_server = imaplib.IMAP4_SSL('imap.example.com')
 
-    if (mail_server.login('email@example.com', 'PASSWORD')[0] != 'OK'):
+def fetch_mails(mail_server, last_uid, last_uid_file):
+    """
+    """
+    if last_uid_file is None:
+        last_uid_file = last_uid
+
+    messages = []
+    # for uid in xrange(last_uid_file, last_uid+1):
+    data = mail_server.uid('fetch', last_uid, '(RFC822)')
+    header_data = data[1][0][1]
+
+    parser = HeaderParser()
+    msg = parser.parsestr(header_data)
+
+    logger.debug("Fetched! From:{}, Subject:{}".format(
+        msg["From"], msg["Subject"]))
+    # messages.append(msg)
+    return msg
+
+
+def check_mails(host, user, password):
+    """
+    """
+    home_path = os.path.expanduser("~")
+    os.chdir(home_path)
+    mail_server = imaplib.IMAP4_SSL(host)
+
+    if (mail_server.login(user, password)[0] != 'OK'):
         exit("no conn")
 
-    # TODO: use home path
     try:
         with open('.imap_check', 'r') as f:
             last_uid_file = f.readline()
             last_uid_file = int(last_uid_file)
     except (IOError, ValueError):
-        # file not found
+        logger.debug("file not found in {}".format(home_path))
         last_uid_file = None
 
     last_email_in_folder = mail_server.select('INBOX')[1][0]
     last_uid = mail_server.uid('search', None, last_email_in_folder)[1][0]
 
     # debug
-    # print last_uid_file, type(last_uid_file)
-    # print last_uid, type(last_uid)
+    logger.debug("last_uid_file:{}, last_uid:{}".format(
+        last_uid_file, last_uid))
 
     if last_uid_file is not None and int(last_uid) <= int(last_uid_file):
         msg = {}
     else:
-        data = mail_server.uid('fetch', last_uid, '(RFC822)')
-        header_data = data[1][0][1]
+        msg = fetch_mails(mail_server, last_uid, last_uid_file)
 
-        parser = HeaderParser()
-        msg = parser.parsestr(header_data)
-
-    # debug
-    # print msg['From']
-    # print msg['Subject']
-
-    # TODO: use home path
     if not last_uid_file or int(last_uid) != int(last_uid_file):
         try:
             with open('.imap_check', 'w') as f:
                 f.write(last_uid)
         except IOError:
-            # Cannot write
-            pass
+            logger.debug("cannot write in file")
 
     return msg
 
+
+# get configuration
+config = ConfigParser.SafeConfigParser()
+# TODO For now we read from local path
 # TODO Catch error
-msg = fetch_mail()
+config.read('config')
+host = config.get('imap', 'host')
+user = config.get('imap', 'user')
+password = config.get('imap', 'password')
+
+msg = check_mails(host, user, password)
 
 # notify
 if msg:
@@ -72,5 +103,12 @@ if msg:
                                          message=msg["Subject"],
                                          icon="emblem-debian")
     notification.show()
+
+    # OSX notification test purposes
+    # from pync import Notifier
+    # Notifier.notify(msg["Subject"][:25], title=msg["From"][:10])
+    # Notifier.remove(os.getpid())
+    # Notifier.list(os.getpid())
 else:
-    print "NO EMAIL"
+    # debug
+    logger.debug("no email")
